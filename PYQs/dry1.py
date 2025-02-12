2693d903224e997e00f48c0aef24264d32175785
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-02-12 20:23:29 +0530

    added files

---
+++ PYQs/dry1.py
@@ -0,0 +1,40 @@
+
+
+# def prime(num):
+#     flag = 0
+#     if num == 1:
+#         print("1 is not a prime number")
+#     elif num == 2:
+#         print("2 is a prime number")
+#     else:
+#         for i in range(2, num+1):
+#             if num % i == 0:
+#                 flag += 1
+#         if flag > 1:
+#             print(f"{num} is not a prime number")
+#         else:
+#             print(f"{num} is a prime number")
+
+# def prime_range(start, end):
+#     for i in range(start, end+1):
+#         prime(i)
+
+# prime_range(1, 10)
+
+
+# def fibonacii(num):
+#     a = 0
+#     b = 1
+#     for i in range(1,num+1):
+#       if i == 1:
+#          print(a)
+#       elif i == 2:
+#          print(b)
+#       else:
+#             c = a + b
+#             print(c)
+#             a = b
+#             b = c
+
+# fibonacii(10)
+

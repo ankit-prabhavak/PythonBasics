81071c03cd4fa440d0bc6269b7e2565aa0bcbc2e
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-03-04 22:39:58 +0530

    added SF1.py

---
+++ AusKaBoss.py
@@ -0,0 +1,14 @@
+# India vs Australia:SF1 Dubai International Stadium LIVE...
+def VandeMatram():
+    print("Chak De India")
+    print("India beats Australia by 4 wickets, they are through to the finals.")
+    print("19 November 2023 Ka Badla Le Liya Re Bhai")
+    print("Champions Trophy 2025")
+    print("See U On 9 March Final Day!")
+
+
+def main():
+    VandeMatram()
+
+# Main function implementation.
+main()

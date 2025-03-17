65168560d9af74e9a745ddda7820ac958bdc5f44
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-03-17 22:16:09 +0530

    added new files

---
+++ PU/dry.py
@@ -0,0 +1,23 @@
+# Find the maximum among three numbers
+'''def findMax(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+    
+
+x = findMax(1, 2, 3)
+print(x)  
+'''
+
+# Write a program to check whether the given string is palindrome or not.
+'''
+def checkPalindrome(s):
+    return s == "".join(reversed(s))
+
+string = "racecar"
+print(checkPalindrome(string)) 
+'''
+

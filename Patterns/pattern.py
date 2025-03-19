8de75677ad0b5d526aea22472468b5f9c562d672
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-03-19 16:24:17 +0530

    added a new pattern

--- Patterns/pattern.py
+++ Patterns/pattern.py
@@ -183,3 +183,31 @@ Output:
 **
 *
 '''
+
+# Pattern 10:
+'''
+def pattern10(n):
+    for i in range(1, n+1):
+        print(" "*(n-i) + "*"*(2*i-1))
+
+    for i in range(n-1, 0, -1):
+        print(" "*(n-i) + "*"*(2*i-1))
+
+pattern10(5)
+'''
+'''
+Output:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+'''
+
+
+
+

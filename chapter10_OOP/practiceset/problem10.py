5e9dc2af4cf75253281608762ef957c48f1f87fb
ankit-prabhavak <ankitabcd1718@gmail.com>
2025-04-16 11:43:43 +0530

    updated oops problem

---
+++ chapter10_OOP/practiceset/problem10.py
@@ -0,0 +1,33 @@
+class Person:
+
+    college = "PSIT"
+
+    def __init__(self, name, birth_year):
+        self.name = name
+        self.birth_year = birth_year
+
+    def get_info(self):
+        print(f"Name: {self.name}, Birth Year: {self.birth_year}, College: {self.college}")
+
+    def calculate_age(self):
+        current_year = int(input("Enter the current year: "))
+        return current_year - self.birth_year
+    
+    def is_adult(self):
+        age = self.calculate_age()
+        if age >= 18:
+            print(f"{self.name} is an adult as they are {age} years old.")
+        else:
+            print(f"{self.name} is not an adult as they are {age} years old.")
+
+
+# Create an object of the Person class
+person1 = Person("John Doe", 2000)
+person1.get_info()
+person1.is_adult()
+
+age = person1.calculate_age()
+print(f"{person1.name} is {age} years old.")
+
+print(f"College: {person1.college}")
+
